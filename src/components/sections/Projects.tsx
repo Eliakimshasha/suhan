92fd7a3994dff04b473d4c0c4cfc6870ba40be67
eliakimshasha <eliakimshasha@gmail.com>
2025-02-24 17:58:58 +0000

    first

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -70,7 +70,7 @@ export default function Projects() {
                     ))}
                   </div>
                   <div >
-                    <LuScanEye size={30} className='bg-black p-2 rounded-full bg-opacity-40'/>
+                    <LuScanEye size={30} className='bg-yellow-100 dark:bg-black p-2 rounded-full bg-opacity-40'/>
                   </div>
                 </div>
               </div>
