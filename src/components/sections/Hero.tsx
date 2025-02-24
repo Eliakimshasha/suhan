2875d033c247943b6676bd3968bc3a5464d18663
eliakimshasha <eliakimshasha@gmail.com>
2025-02-24 14:55:15 +0000

    first

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -15,14 +15,14 @@ export default function Hero() {
           transition={{ duration: 0.8 }}
           className="text-center"
         >
-          <motion.h1
-            className="text-6xl flex justify-center items-center font-bold mb-2 bg-clip-text text-center md:text-center lg:text-center text-transparent bg-gradient-to-r from-yellow-400 to-blue-400"
+          <motion.div
+            className="text-6xl flex justify-center items-center font-bold mb-0 bg-clip-text text-center md:text-center lg:text-center text-transparent bg-gradient-to-r from-yellow-400 to-blue-400"
             animate={{ scale: [1, 1.02, 1] }}
             transition={{ repeat: Infinity, duration: 4 }}
           >
             {/* Suhan Creatives */}
             <Image src={Logo} height={100} width={100}  className='w-[500px] h-auto' alt='Suhan Creatives'/>
-          </motion.h1>
+          </motion.div>
           <motion.p
             className="text-xl mb-3 text-gray-700 dark:text-gray-300"
             initial={{ opacity: 0 }}
@@ -31,7 +31,7 @@ export default function Hero() {
           >
             Transforming ideas into digital experiences
           </motion.p>
-          <Button className="bg-yellow-400 hover:bg-yellow-500 text-white px-8 py-3 rounded-full text-lg font-semibold transition-all duration-300 hover:scale-105">
+          <Button className="bg-yellow-400 hover:bg-yellow-500 text-white px-8 py-3 mb-4 rounded-full text-lg font-semibold transition-all duration-300 hover:scale-105">
             Get in Touch
           </Button>
         </motion.div>
